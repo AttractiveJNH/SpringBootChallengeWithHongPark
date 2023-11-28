8b19b8287d3991473eb73a527e7089c8695f022c
AttractiveJNH <141652584+AttractiveJNH@users.noreply.github.com>
2023-11-28 22:41:32 +0900

    Day7
    셀프체크만 하면 끝!

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -11,6 +11,8 @@ import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 //생성자가 자동으로 추가되는 어노테이션이다.
 @ToString
 public class ArticleForm {
+
+    private Long id;            // id 필드 추가
     private String title;       // 제목을 받을 필드
     private String content;     // 내용을 받을 필드
 
@@ -18,7 +20,7 @@ public class ArticleForm {
     public Article toEntity() {
         // DTO인 form 객체를 entity 객체로 변환하는 역할을 한다.
 
-        return new Article(null, title, content);
-        // id는 자동생성이라 null을 반환해야한다.
+        return new Article(id, title, content);
+
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUES (1, '오늘날씨추우네', '반팔 그만입기');
+INSERT INTO article(id, title, content) VALUES (2, '추울땐 팥빙수', '먹으면 죽어');
+INSERT INTO article(id, title, content) VALUES (3, '오늘 먹은 우동', '망했다 체한건가');
